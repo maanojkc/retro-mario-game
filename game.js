70e46e83e51ad737645d4baae10c1b748f73eb74
Manoj <manojkc8431@gmail.com>
2026-04-05 10:04:48 +0530

    Update game.js

--- game.js
+++ game.js
@@ -8,22 +8,32 @@ let x = 50;
 let y = 200;
 let velocity = 0;
 let gravity = 0.5;
+let ground = 200;
 
-document.addEventListener("keydown", () => {
-  velocity = -10;
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    velocity = -10;
+  }
 });
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // gravity
   velocity += gravity;
   y += velocity;
 
-  if (y > 200) {
-    y = 200;
+  // ground collision
+  if (y > ground) {
+    y = ground;
     velocity = 0;
   }
 
+  // draw ground
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 230, canvas.width, 70);
+
+  // draw player
   ctx.fillStyle = "red";
   ctx.fillRect(x, y, 30, 30);
 
